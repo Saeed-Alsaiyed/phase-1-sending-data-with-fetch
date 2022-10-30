debbb11316e426557b6cd0139f4dc6ff438a5e97
Saeed-Alsaiyed <msalsaiyed0@gmail.com>
2022-10-30 23:19:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+let body = document.querySelector('body');
+function submitData(username, emailMessag){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: username,
+          email: emailMessag,
+        })
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        body.append(object.id);
+    })
+    .catch(function (error) {
+        alert("error")
+        body.append(error.message);
+     });
+
+    
+}
+
